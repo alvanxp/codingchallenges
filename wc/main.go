aa592fab3e89bfbec105006563cdc4d4115568ab
Alvaro Carpio <alvanxp@outlook.com>
2024-05-05 21:21:25 -0500

    chore: Improve command line argument handling and file processing in main.go

--- wc/main.go
+++ wc/main.go
@@ -9,32 +9,37 @@ import (
 
 func main() {
 
-	filename := getFilePath()
+	filename, err := getFilePath()
+	if err != nil {
+		fmt.Println("Error processing file:", err)
+		return
+	}
 	showByteCount := flag.Bool("c", false, "show the number of bytes in the file")
 	showLinesCount := flag.Bool("l", false, "show the number of lines in the file")
 	showWordsCount := flag.Bool("w", false, "show the number of words in the file")
 	flag.Parse()
 
-	var byteCount, linesCount, wordsCount int = 0,0,0
+	var byteCount, linesCount, wordsCount int = 0, 0, 0
 
 	var consoleOutput string
 	data, err := getDataFromFile(filename)
 	if err != nil {
 		fmt.Println("Error processing file:", err)
+		return
 	}
 	if *showByteCount {
 		byteCount = data.GetBytesCount()
-		consoleOutput = AddCounterFormat(byteCount, consoleOutput)
+		consoleOutput = AppendCounterFormat(byteCount, consoleOutput)
 	}
 
 	if *showLinesCount {
 		linesCount = data.GetLinesCount()
-		consoleOutput = AddCounterFormat(linesCount, consoleOutput)
+		consoleOutput = AppendCounterFormat(linesCount, consoleOutput)
 	}
 
 	if *showWordsCount {
 		wordsCount = data.GetWordsCount()
-		consoleOutput = AddCounterFormat(wordsCount, consoleOutput)
+		consoleOutput = AppendCounterFormat(wordsCount, consoleOutput)
 	}
 
 	consoleOutput = fmt.Sprintf("%s %s", consoleOutput, filename)
@@ -42,7 +47,7 @@ func main() {
 	fmt.Println(consoleOutput)
 }
 
-func AddCounterFormat(count int, input string) string {
+func AppendCounterFormat(count int, input string) string {
 	return fmt.Sprintf("%s %d", input, count)
 }
 
@@ -55,16 +60,13 @@ func getDataFromFile(filename string) (fileData, error) {
 	return data, nil
 }
 
-func getFilePath() string {
-	for i := range os.Args {
-		if i == 0 {
-			continue
-		}
+func getFilePath() (string, error) {
+	for i := 1; i < len(os.Args); i++ {
 		if os.Args[i][0] != '-' {
-			return os.Args[i]
+			return os.Args[i], nil
 		}
 	}
-	return ""
+	return "", fmt.Errorf("No file path provided")
 }
 
 type fileData []byte
